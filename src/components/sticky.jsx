627695b7da5f93adb64bcc8ca62b76754d65d0ff
Hanhee Song <song.hanhee@gmail.com>
2017-12-09 21:29:39 -0500

    add sticky and sticky_index

--- src/components/sticky.jsx
+++ src/components/sticky.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class Sticky extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      content: this.props.content,
+      editing: false,
+    };
+    this.enableEdit = this.enableEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  enableEdit(field) {
+    return () => {
+      this.setState({ editing: field });
+    };
+  }
+  
+  handleChange(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+  
+  handleSubmit() {
+    this.props.updateSticky(this.state.title, this.state.content);
+  }
+  
   render () {
     return(
-      <div>
-        this is a sticky
+      <div className="sticky">
+        {
+          !this.state.editing ?
+          (
+            <div className="sticky-title"
+              onClick={this.enableEdit("title")}>
+              {this.state.title}
+            </div>
+          ) : (
+            <form className="sticky-title form"
+              onSubmit={this.handleSubmit}>
+              <input
+                autoFocus={this.state.editing === "title"}
+                onChange={this.handleChange("title")}
+                value={this.state.title}
+                />
+            </form>
+          )
+        }
+        {
+          !this.state.editing ?
+          (
+            <div className="sticky-content"
+              onClick={this.enableEdit("content")}>
+              {this.state.content}
+            </div>
+          ) : (
+            <form className="sticky-content form"
+              onSubmit={this.handleSubmit}>
+              <input
+                autoFocus={this.state.editing === "content"}
+                onChange={this.handleChange("content")}
+                value={this.state.content}
+                />
+            </form>
+          )
+        }
       </div>
     );
   }
--- src/components/sticky_index.jsx
+++ src/components/sticky_index.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Sticky from './sticky';
 
 class StickyIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stickies: [
+        {
+          title: "sticky 1",
+          content: "sticky 1 content"
+        },
+        {
+          title: "sticky 2",
+          content: "sticky 2 content"
+        }
+      ]
+    };
+    this.updateSticky = this.updateSticky.bind(this);
+  }
+  
+  updateSticky(title, content) {
+    return () => {
+      
+    };
+  }
+  
   render () {
+    const stickies = this.state.stickies.map((sticky, i) => {
+      return (
+        <Sticky
+          key={i}
+          title={sticky.title}
+          content={sticky.content}
+          updateSticky={this.updateSticky(i)}/>
+      );
+    });
+    
     return(
-      <div>
-        This is a sticky index
-        <Sticky />
+      <div className="sticky-index">
+        {stickies}
       </div>
     );
   }
